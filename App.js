5f60b962e9c7299bcd180f7e3b91405346851b3d
hoseinmahmoudi <55023775+hoseinmahmoudi@users.noreply.github.com>
2023-04-09 22:55:12 +0330

    flex

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ width: 100, backgroundColor: "blue", height: 100 }} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
